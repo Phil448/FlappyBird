0a367fca3ef9a2d41b038c9e532732b5ff337508
Phil448 <72897958+Phil448@users.noreply.github.com>
2023-11-24 22:09:53 -0400

    Add files via upload

---
+++ Assets/TreeScript.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeScript : MonoBehaviour
+{
+    public float JumpStrength;
+
+    public Rigidbody2D myrigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {/*
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            myrigidbody.velocity = Vector2.right * JumpStrength;
+        }
+     
+        
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            myrigidbody.velocity = Vector2.left * JumpStrength;
+        }
+        */
+        
+        if (Input.GetKeyDown(KeyCode.Space) == true)
+        {
+            myrigidbody.velocity = Vector2.up * JumpStrength;
+
+        }
+    }
+}
